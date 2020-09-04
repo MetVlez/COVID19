12ae243c33921b34b15adc052378cd940e3ef642
Met <metvlez01@outlook.com>
2020-09-04 16:30:04 -0500

    filtro

--- filtro.py
+++ filtro.py
@@ -7,10 +7,8 @@ from operator import attrgetter
 DB = pd.read_csv("covid19_tweets.csv")
 print("Las columnas son",DB.keys())
 #print(pd.read_csv("covid19_tweets.csv")
-#print("FECHAS",DB.date)
-DB=DB.sort_values(('date'),ascending=(False)
+print("FECHAS",type(DB.date[1]))
+
+DB=DB.sort_values( 'date', ascending=False )
 
 #print("la fecha más antigua es:", DB.sort_values(('date'),ascending=(False))
-
-
-
