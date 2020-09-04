d7cb8d6665a777a5073ca1b683932862744f6936
Natan Vilchis <contacto@natanvilchis.org>
2020-09-04 16:49:35 -0500

    filter

--- filtro.py
+++ filtro.py
@@ -7,12 +7,13 @@ from operator import attrgetter
 DB = pd.read_csv("covid19_tweets.csv")
 print("Las columnas son",DB.keys())
 #print(pd.read_csv("covid19_tweets.csv")
+print(DB)
 print("FECHAS",type(DB.date[1]))
 print("Hola :D")
 # aaaa-mm-dd hh:mm:ss
 # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
 DB.date = pd.to_datetime(DB.date, format="%Y-%m-%d %H:%M:%S")
 DB=DB.sort_values( 'date', ascending=True )
-print(DB.date.to_string(),len(DB.date.to_string()))
+print(DB.date)
 
 #print("la fecha más antigua es:", DB.sort_values('date',ascending=False))
