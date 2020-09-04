2ed54e24135f15b3eb2418ae4d7f8d8b62d33366
Met <metvlez01@outlook.com>
2020-09-04 17:30:17 -0500

    052

--- 052.py
+++ 052.py
@@ -7,4 +7,13 @@ from operator import attrgetter
 
 DB = pd.read_csv("covid19_tweets.csv")
 print("Las columnas son",DB.keys())
-print(DB["user_location"])
+#print(DB["user_location"])
+#Lizeth
+locations = list(set(DB["user_location"].values))
+for location in locations:
+    print(location)
+
+#Tener un listado tuyo de paises y siglas
+paises = 
+
+
