67273920a757bfbcaddf99e596bd9a778e12d2f4
Natan Vilchis <contacto@natanvilchis.org>
2020-09-05 00:50:30 -0500

    Toppers

--- pruebas.py
+++ pruebas.py
@@ -8,13 +8,22 @@ DB = pd.read_csv("covid19_tweets.csv")
 print("Las columnas son",DB.keys())
 #print("usuario",type(DB.user_name[1]))
 
-
+# Se inicializan valores
 usuarios = {}
 for usuario in list(set(DB["user_name"].values)):
   usuarios[usuario] = 0
+
+# Se cuentas los tweets por usuario
 for user_name in DB["user_name"].values:
   usuarios[user_name] += 1
 
-for usuario in usuarios:
-  #print(usuario,".....Total de tweets por usuario:",usuarios[usuario])
-  print(max(usuarios,key=lambda i: usuarios[i]))
+# se obtiene el usuario que hizo más tweets
+usuarioMax = max(usuarios,key=lambda i: usuarios[i])
+print(usuarioMax,"total de tweets= {}".format(usuarios[usuarioMax]))
+
+#Hacemos top 10
+print("se hace top 10:")
+for i in range(10):
+  usuarioMax = max(usuarios,key=lambda i: usuarios[i])
+  print("#{}".format(i+1),usuarioMax,"total de tweets= {}".format(usuarios[usuarioMax]))
+  usuarios.pop(usuarioMax)  
